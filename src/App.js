6f7f737d87f349d30917590b59bdf1432911c95d
Nikhil Vishwakarma <122264427+Nikhilv47@users.noreply.github.com>
2023-07-26 21:56:07 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Switch,Routes , Route, Router, BrowserRouter } from 'react-router-dom';
 import Login from './components/Login'
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -8,6 +10,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<Login/>}/>
+        <Route exact path='/home' element={<>
+        <Header/>
+        <Home/>
+        </>}/>
+
       </Routes>
     </BrowserRouter>
     </div>
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+const Home = ()=>{
+
+    return(
+  <Container>
+    <Section>
+        <h5><a >Hiring in a hurry? -</a><p>Find talented pros in record time with upwork and keep business moving</p></h5>
+    </Section>
+  </Container>
+    )};
+
+const Container = styled.div`
+`;
+
+const Content = styled.div``;
+
+const Section = styled.section`
+
+`;
+
+export default Home ;
